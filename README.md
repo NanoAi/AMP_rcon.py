5b8a1d10c34759c86a9242e912655e947b57b7e6
NanoAi <tenshi@mirai.red>
2020-04-22 17:57:19 -0400

    Added rcon.py
    I hope this makes peoples lives easier. Tips and general improvements are appreciated!

--- README.md
+++
@@ -1,2 +0,0 @@
-# AMP_rcon.py
-A python script to make rcon requests to AMP through their API. This makes it easy to run console commands from the command line. :)
---
+++ rcon.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+import json, sys, requests, getpass
+
+# Set this variable to your port number if you don't want to define the port all the time!
+PORT = ""
+# Set this variable to define the file your SessionID will be written to.
+# This is so you don't have to define it all the time.
+WritePath = "/tmp/amp_sessionid"
+
+# API-EndPoints
+Login = "http://localhost:%port%/API/Core/Login"
+SendMessage = "http://localhost:%port%/API/Core/SendConsoleMessage"
+
+# Headers
+Headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
+
+# Data
+Data = {}
+
+if len(sys.argv) <= 0:
+	print("Type in `-l` to start, then enter the commands you'd like to run.")
+
+if sys.argv[1].lower()[:2] == "-l":
+	Username = len(sys.argv) > 2 and sys.argv[2] or input("Username: ")
+	Password = len(sys.argv) > 3 and sys.argv[3] or getpass.getpass("Password: ")
+
+	RememberMe = len(sys.argv) > 4 and sys.argv[4] or str(
+		input("Remember Me [ Y / [N] ]: ") ).lower()[:0] == "y"
+
+	Port = PORT or input("Port: ")
+	Login = Login.replace( "%port%", Port, 1 )
+
+	# Define the data we want to send.
+	Data = { "username": Username, "password": Password,
+			"token": "", "rememberMe": str(RememberMe).lower() }
+
+	# Send the post request, and output the response to a variable.
+	r = requests.post(url=Login, data=json.dumps( Data ), headers=Headers)
+
+	f = open(WritePath, "w")
+	f.write(r.json()['sessionID'])
+	f.close()
+elif sys.argv[1].lower()[:2] == "-d":
+  # Overwite the file, thus removing its contents.
+	f = open(WritePath, "w")
+	f.write("")
+	f.close()
+	print("RCON Disconnected.")
+else:
+	with open(WritePath, 'r') as file:
+		key = file.read().replace('\n', '')
+
+	Port = PORT or input("Port: ")
+	SendMessage = SendMessage.replace("%port%", Port, 1)
+
+	Data = {'SESSIONID': key, 'message': ' '.join(sys.argv[1:])}
+	r = requests.post(url=SendMessage, data=json.dumps(Data), headers=Headers)
